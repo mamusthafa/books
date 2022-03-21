c7431888f1fc4354832368bbac7908937425d195
unaisphads <95120114+unaisphads@users.noreply.github.com>
2022-03-21 16:10:29 +0530

    initial authors routes

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
 const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bodyParser = require('body-parser')
 
 app.set('view engine','ejs')
 app.set('views', __dirname+'/views')
 app.set('layout','layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
+app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
 
 //mongodb connection
 const mongoose = require('mongoose')
@@ -25,5 +28,6 @@ db.once('open',()=>console.log('Connected to mongoose'))
 
 
 app.use('/', indexRouter)
+app.use('/authors', authorRouter)
 
 app.listen(process.env.PORT || 3000)
