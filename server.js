9c19034caa11c44b280cbac0b9bbd1e6dc3430a4
unaisphads <95120114+unaisphads@users.noreply.github.com>
2022-03-22 11:51:50 +0530

    initial books routes

--- server.js
+++ server.js
@@ -1,33 +1,31 @@
-//checking whether it is production or development
-if(process.env.NODE_ENV !=='production'){
+if (process.env.NODE_ENV !== 'production') {
   require('dotenv').config()
-} //checking whether it is production or development
+}
 
 const express = require('express')
 const app = express()
 const expressLayouts = require('express-ejs-layouts')
-const indexRouter = require('./routes/index')
-const authorRouter = require('./routes/authors')
 const bodyParser = require('body-parser')
 
-app.set('view engine','ejs')
-app.set('views', __dirname+'/views')
-app.set('layout','layouts/layout')
+const indexRouter = require('./routes/index')
+const authorRouter = require('./routes/authors')
+const bookRouter = require('./routes/books')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
 app.use(expressLayouts)
 app.use(express.static('public'))
-app.use(bodyParser.urlencoded({limit:'10mb',extended:false}))
+app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
 
-//mongodb connection
 const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL,{useNewUrlParser:true})
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
 const db = mongoose.connection
-db.on('error',error=>console.error(error))
-db.once('open',()=>console.log('Connected to mongoose'))
-//mongodb connection 
-
-
+db.on('error', error => console.error(error))
+db.once('open', () => console.log('Connected to Mongoose'))
 
 app.use('/', indexRouter)
 app.use('/authors', authorRouter)
+app.use('/books', bookRouter)
 
 app.listen(process.env.PORT || 3000)
